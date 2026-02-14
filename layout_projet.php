7d5ea16ffb2c6d54fb4a71ec877835448e076d2a
Yume_88 <octavelejeune80@gmail.com>
2026-02-14 21:01:37 +0100

    début du php avec nouvelles pages de projet et include pour le layout

---
+++ layout_projet.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body class="page-body">
+    <div class="wrapper-projet">
+            <div class="background-wrapper">
+        <div class="background-glow"></div>
+    </div>
+
+    </div>
+</body>
+</html>
---
+++ projet_aide_et_vous.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet - Aide&Vous</title>
+</head>
+<body>
+        <?php
+            include 'layout_projet.php';
+        ?>
+</body>
+</html>
